d8caf4600ac8e4d6b8ddf998ee0098224d4ee658
bukacee <itzbuka@gmail.com>
2026-01-06 07:13:27 +0100

    Updated

--- project/lib/pages/create_reels_page/controller/create_reels_controller.dart
+++ project/lib/pages/create_reels_page/controller/create_reels_controller.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:camera/camera.dart';
 import 'package:deepar_flutter/deepar_flutter.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
